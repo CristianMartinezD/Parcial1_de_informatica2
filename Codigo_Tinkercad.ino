7915b39d4007317d4595ce4ef45c9e8390422f22
CristianMartinezD <80220614+CristianMartinezD@users.noreply.github.com>
2021-04-22 17:26:12 -0500

    Update Codigo_Tinkercad.ino
    En este momento acababa de construir bien las funciones que necesitaba.

--- Codigo_Tinkercad.ino
+++ Codigo_Tinkercad.ino
@@ -1,4 +1,3 @@
-
 void setup()
 {
   Serial.begin(9600);
@@ -12,16 +11,29 @@ void setup()
 
 int condi = 0;
 int TotalF;
-int *Matriz;
-int figuras[64][64] = {};
+int *vector;
+
 int DATOS[2][3];
 void loop()
 {
   if(condi == 0){
     Serial.println("Ingresa # de figuras");
     TotalF = Totalfiguras();
+    int figuras[TotalF][64] = {};
     
-    Matriz = ConstruirMatrizDeElem(DATOS);
+    for(int i = 0; i < TotalF; i++){
+      Serial.println("INGRESA TODOS LRDS DE LA FIGURA #"+String(i+1));
+      vector = construirArray();
+      	
+      for(int j = 0; j < 64; j++){
+        figuras[i][j] = vector[j];
+      }
+    }
+    
+    
+    
+    
+    
     condi = 1;
   }
   
@@ -79,7 +91,7 @@ int Totalfiguras(){
 
 
 
-/*int* construirArray(){
+int* construirArray(){
   
   static int resul[64]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
   int numeros[64] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64};
@@ -88,25 +100,27 @@ int Totalfiguras(){
   int ban = 2;
   int LED;
   while(ban != 0){
+   cont += 1;
+   Serial.println(" Ingresa el led #"+String(cont)+" o ingresa 0 para fializar.");
     
    LED = LEDS();
   
    if(LED==0){ban = 0;}
    else if(LED != 0){
       for(int i = 0; i < 64; i++){
-         if(numeros[i] == LED){resul[63-i] = 1; cont += 1;}
+         if(numeros[i] == LED){resul[63-i] = 1;}
                                    }
       }
-     if(dato != 0){
+     if(LED != 0){
       	Serial.print("hemos agregado el led #"+String(cont));
         Serial.println(" Ingresa otro o ingresa 0 para fializar.");}
       
     }
   return resul;
-}*/
+}
 
 
-void ConstruirMatrizDeElem(int DATOS[2][3]){
+/*void ConstruirMatrizDeElem(int DATOS[2][3]){
  
   //DATOS[0][0]= 4;
   int *array;
@@ -129,10 +143,10 @@ void ConstruirMatrizDeElem(int DATOS[2][3]){
     for(int j =0; j < 64; j++){
       DATOS[F][j] = array[j];}// esto esta construyendo una matriz donde cada posicion tiene el array recibido anteriormente.
   }
-   */  
+    
     int DATO;
     //return DATOS; 
-}
+}*/
   
 
 int LEDS(){
@@ -140,9 +154,11 @@ int LEDS(){
   int LED;
   while(ban == 0){
     if(Serial.available()){
+      Serial.println("Por favor espera un momento......");
        LED = Serial.parseInt();
       ban = 1;
     }
   }
+  
   return LED;
 }
